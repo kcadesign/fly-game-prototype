5eedf18d1089f323473ba8d32e163aa724fff123
kcadesign <kace85@hotmail.com>
2023-04-16 12:48:04 +0900

    Change player movement to force / physics based

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,42 +4,113 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    [Header("References")]
+    private Rigidbody rigidBody;
 
-    private float horizontalInput;
-    private float verticalInput;
-    private Rigidbody rb;
+    [Header("Speed Values")]
+    [SerializeField] private float lateralSpeed = 5f;
+    [SerializeField] private float verticalSpeed = 5f;
+    [SerializeField] private float mouseLookSpeed = 1f;
+
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void FixedUpdate()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        HandleMovement();
 
-        // Move forward or backward
-        Vector3 forwardForce = transform.forward * verticalInput * speed;
-        rb.AddForce(forwardForce, ForceMode.Impulse);
+        HandleMouseLook();
+    }
 
-        // Strafe left and right
-        Vector3 lateralForce = transform.right * horizontalInput * speed;
-        rb.AddForce(lateralForce, ForceMode.Impulse);
+    private void HandleMovement()
+    {
+        HandleForwardMovement();
+        HandleLateralMovement();
+        HandleVerticalMovement();
 
         /*
-        // Turn left or right
-        float turnAmount = horizontalInput * turnSpeed * Time.deltaTime;
-        Quaternion turnOffset = Quaternion.Euler(0f, turnAmount, 0f);
-        rb.MoveRotation(rb.rotation * turnOffset);
+        // Move forward or backward
+        transform.position += speed * Time.deltaTime * verticalInput * transform.forward;
+
+        // Strafe left and right
+        transform.position += horizontalInput * speed * Time.deltaTime * transform.right;
         */
 
-        
+        // Move forward or backward with physics force
+
+        // Strafe left and right with physics force
+
+        // Move vertically with physics force
+
     }
+
+    private void HandleForwardMovement()
+    {
+        float forwardInput = Input.GetAxisRaw("Vertical");
+        Vector3 forwardForce = lateralSpeed * forwardInput * transform.forward;
+
+        if (forwardInput != 0)
+        {
+            rigidBody.AddForce(forwardForce, ForceMode.Impulse);
+        }
+        else
+        {
+            forwardForce = Vector3.zero;
+            rigidBody.AddForce(-rigidBody.velocity, ForceMode.Impulse);
+        }
+    }
+
+    private void HandleLateralMovement()
+    {
+        float lateralInput = Input.GetAxisRaw("Horizontal");
+        Vector3 lateralForce = lateralSpeed * lateralInput * transform.right;
+
+        if (lateralInput != 0)
+        {
+            rigidBody.AddForce(lateralForce, ForceMode.Impulse);
+        }
+        else
+        {
+            lateralForce = Vector3.zero;
+            rigidBody.AddForce(-rigidBody.velocity, ForceMode.Impulse);
+        }
+    }
+
+    private void HandleVerticalMovement()
+    {
+        float verticalInput = Input.GetAxisRaw("Jump");
+        Vector3 verticalForce = verticalSpeed * verticalInput * transform.up;
+
+        if (verticalInput != 0)
+        {
+            rigidBody.AddForce(verticalForce, ForceMode.Impulse);
+        }
+        else
+        {
+            verticalForce = Vector3.zero;
+            rigidBody.AddForce(-rigidBody.velocity, ForceMode.Impulse);
+        }
+    }
+
 
     private void HandleMouseLook()
     {
+        float mouseX = Input.GetAxis("Mouse X") * mouseLookSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseLookSpeed;
 
+        // Rotate the player object around its y-axis (left and right)
+        transform.Rotate(0f, mouseX, 0f);
+
+        // Rotate the player object around its x-axis (up and down)
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        float newRotationX = currentRotation.x - mouseY;
+        transform.rotation = Quaternion.Euler(newRotationX, currentRotation.y, 0);
     }
+
 }
